0710c9eb3f11f8cb24c1f5d6743f3c48b03ddb08
Rutuja <patil.rut@northeastern.edu>
2023-10-18 21:24:37 -0400

    oo

--- integration-tests/integration-tests.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../server'); 
-
-describe('/healthz endpoint', () => {
-    it('should return a 200 OK status', async () => {
-        const response = await request(app).get('/healthz');
-        expect(response.statusCode).toBe(200);
-    });
-});
--- server.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js'
-
-const port = 8080;
- 
-app.listen(port,() =>{
-    console.log(`health check app listening on port ${port}`);
-
-});
