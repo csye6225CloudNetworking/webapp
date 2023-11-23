75e2e44b9b05ac11d2303ca549e10da84b7fdbc3
Rutuja <patil.rut@northeastern.edu>
2023-11-23 17:44:04 -0500

    updated

--- routes/assign-route.js
+++ routes/assign-route.js
@@ -4,11 +4,13 @@ import * as assignController from '../controller/assign-controller.js';
 import * as healthController from '../controller/health-controller.js';
 import {tokenize} from '../service/auth.js';
 
-
 const router = express.Router();
 
 // POST /assignments route to create an assignment
 router.route('/v1/assignments').post(tokenize, assignController.createAssignment);
+
+// POST /assignments/:id/submit route to submit an assignment
+router.post('/v1/assignments/:id/submission', tokenize, assignController.submitAssignment);
 
 // GET /assignments route to retrieve assignments created by the authenticated user
 router.get('/v1/assignments', tokenize, assignController.getAssignmentsByUser);
@@ -26,40 +28,6 @@ router.patch('/v1/assignments',assignController.handlePatch);
 
 router.patch('/v1/assignments/:id',assignController.handlePatch);
 
-
-
-//healthRoute
-// GET 
 router.get('/healthz', );
-
-
-/* const Assignment = require('../models/assign-model.js'); // Import your Assignment model
-
-function authenticateBasicAuth(req, res, next) {
-    const credentials = basicAuth(req);
-  
-    if (!credentials || !credentials.name || !credentials.pass) {
-      res.setHeader('WWW-Authenticate', 'Basic realm="Authentication required"');
-      return res.sendStatus(401); // Unauthorized
-    }
-} */
-
-// GET /assignments route
-/* router.get('/assignments', authenticateBasicAuth, async (req, res) => {
-  const createdBy = req.user.username;
-  try {
-    // Find all assignments created by the authenticated user
-    const assignments = await Assignment.findAll({
-      where: {
-        createdBy,
-      },
-    });
-
-    res.json(assignments);
-  } catch (error) {
-    console.error(error);
-    res.status(401).json({ message: 'Unauthorized!' });
-  }
-}); */
 
 export default router;
